d231484a4f8356275b23d3d04a5e2d0d746c2377
komsky <komsky@gmail.com>
2025-07-09 10:26:29 +0100

    mvc and angular projects scaffold

---
+++ src/RacingDigital.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RacingDigital.Api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
